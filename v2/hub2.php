f7b6534c1be2874a4eeb85b7014f14eeec7f0d94
Camille Zapata <camillezapata@gmail.com>
2012-11-20 13:48:18 +0800

    some SERP changes, 1 minor header change (fix for iPad)

---
+++ v2/hub2.php
@@ -0,0 +1,73 @@
+<?php require_once('header.php'); ?>
+<?php require_once('widgets/notice-shopsmarter.php'); ?>
+
+<div id="content" class="hub">
+    <div class="row">
+            <div class="twelve columns">
+
+                <!-- CRUMBS NAVIGATION: so people know where they are -->
+                <nav id="crumbs">
+                    <a href="#" class="home">Home</a> &rsaquo; 
+                    <a href="#" class="home">Moms</a> &rsaquo; 
+                    <a href="#" class="current-page">Diapers</a>
+                </nav>
+
+
+                <?php require_once('widgets/featured-diapers.php'); ?>
+
+                <div class="group">
+
+                    <ul class="pagination">
+                      <li class="arrow unavailable"><a href="">&laquo;</a></li>
+                      <li class="current"><a href="">1</a></li>
+                      <li><a href="">2</a></li>
+                      <li><a href="">3</a></li>
+                      <li><a href="">4</a></li>
+                      <li class="unavailable"><a href="">&hellip;</a></li>
+                      <li><a href="">12</a></li>
+                      <li><a href="">13</a></li>
+                      <li class="arrow"><a href="">&raquo;</a></li>
+                    </ul>
+
+                </div>
+
+            </div><!-- ninecol group -->
+
+    </div><!-- row -->
+    <div id="hub-categories" class="row">
+      <div class="three columns">
+         - diapers - 
+      </div>
+      <div class="three columns">
+         - wipes - 
+      </div>
+      <div class="three columns">
+         - changing pads - 
+      </div>
+      <div class="three columns">
+         - ointments and creams - 
+      </div>
+    </div><!-- row -->
+    <div class="row">
+      <div class="three columns">
+         - formula and baby food - 
+      </div>
+      <div class="three columns">
+         - feeding and nursing - 
+      </div>
+      <div class="three columns">
+         - baby bath - 
+      </div>
+    </div><!-- row -->
+</div><!-- content -->
+
+<div id="about-tab">
+  
+  <a href="#" class="tab-label">What is Save22?</a>
+
+  <article>
+
+  </article>
+</div>
+
+<?php require_once('footer.php'); ?>
--- v2/serp.php
+++ v2/serp.php
@@ -1,33 +1,34 @@
 <?php require_once('header.php'); ?>
+<?php require_once('widgets/notice-shopsmarter.php'); ?>
 
 <div id="content" class="search">
     <div class="row">
 
       <div class="nine columns">
-                <!-- CRUMBS NAVIGATION: so people know where they are -->
-                <nav id="crumbs">
-                    <a href="#" class="home">Home</a> &rsaquo; 
-                    <a href="#" class="current-page">Search</a>
-                </nav>
+        <!-- CRUMBS NAVIGATION: so people know where they are -->
+        <nav id="crumbs">
+          <a href="#" class="home">Home</a> &rsaquo; 
+          <a href="#" class="current-page">Search</a>
+        </nav>
 
 
-                <?php require_once('widgets/search-results.php'); ?>
+        <?php require_once('widgets/search-results.php'); ?>
 
-                <div class="group">
+        <div class="group">
 
-                    <ul class="pagination">
-                      <li class="arrow unavailable"><a href="">&laquo;</a></li>
-                      <li class="current"><a href="">1</a></li>
-                      <li><a href="">2</a></li>
-                      <li><a href="">3</a></li>
-                      <li><a href="">4</a></li>
-                      <li class="unavailable"><a href="">&hellip;</a></li>
-                      <li><a href="">12</a></li>
-                      <li><a href="">13</a></li>
-                      <li class="arrow"><a href="">&raquo;</a></li>
-                    </ul>
+            <ul class="pagination">
+              <li class="arrow unavailable"><a href="">&laquo;</a></li>
+              <li class="current"><a href="">1</a></li>
+              <li><a href="">2</a></li>
+              <li><a href="">3</a></li>
+              <li><a href="">4</a></li>
+              <li class="unavailable"><a href="">&hellip;</a></li>
+              <li><a href="">12</a></li>
+              <li><a href="">13</a></li>
+              <li class="arrow"><a href="">&raquo;</a></li>
+            </ul>
 
-                </div>
+        </div>
 
       </div><!-- ninecol group -->
 
