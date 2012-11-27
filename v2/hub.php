a5b8e80a6dcda516e1aeffb1e89a06c9453329ea
Camille Zapata <camillezapata@gmail.com>
2012-11-27 08:29:41 +0800

    IF/ELSE: if Hero Shot, show default html. else: show banner (modified code)

--- v2/hub.php
+++ v2/hub.php
@@ -1,20 +1,12 @@
 <?php require_once('header.php'); ?>
-<?php require_once('widgets/hero.php'); ?>
+<!-- HERO SHOT MOVED HERE --> 
+<?php require_once('widgets/hero.php'); ?> 
 <?php require_once('widgets/notice-shopsmarter.php'); ?>
 
 
 <div id="content" class="hub">
   <div class="row">
     <div class="twelve columns">
-      <!-- 
-      <section>
-        <div id="featured-header" class="block group" style="background:url('img/hero-gadgets.png');">
-          <div class="intro-copy right relative">
-            <a href="#" class="button radius red-button block mtop">Get them now</a>
-          </div>
-        </div>
-      </section>
-      -->
       <h2 class="single-header page-header no-border-top">Featured Items</h2>
 
       <?php require_once('widgets/featured-diapers.php'); ?>
