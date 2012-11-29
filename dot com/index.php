f11f27bccb8bfcd35f2ca967423d40c82ad313b0
Camille Zapata <camillezapata@gmail.com>
2012-11-29 13:44:36 +0800

    added dot com files

---
+++ dot com/index.php
@@ -0,0 +1,14 @@
+<?php require_once('header.php'); ?>
+
+<div id="phone-preview">
+   <div class="row">
+      <div id="iphone">
+         <img src="img/sample-iphone.png" width="300">
+      </div>
+   </div>
+</div>
+<?php require_once('widgets/hero.php'); ?>
+
+
+
+<?php require_once('footer.php'); ?>
