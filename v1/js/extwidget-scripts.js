4e7c87e828ce84a02a63a9cbb7c214a94c25eafd
Camille Zapata <camillezapata@gmail.com>
2012-12-18 13:18:25 +0800

    forgot to add extwidget-scripts.js file from v2 to v1 folder

--- v1/js/extwidget-scripts.js
+++ v1/js/extwidget-scripts.js
@@ -1,15 +1,70 @@
+/*
+Bones Scripts File
+Author: Eddie Machado
+*/
+
+// IE8 ployfill for GetComputed Style (for Responsive Script below)
+if (!window.getComputedStyle) {
+    window.getComputedStyle = function(el, pseudo) {
+        this.el = el;
+        this.getPropertyValue = function(prop) {
+            var re = /(\-([a-z]){1})/g;
+            if (prop == 'float') prop = 'styleFloat';
+            if (re.test(prop)) {
+                prop = prop.replace(re, function () {
+                    return arguments[2].toUpperCase();
+                });
+            }
+            return el.currentStyle[prop] ? el.currentStyle[prop] : null;
+        }
+        return this;
+    }
+}
 
 jQuery(document).ready(function($) {
-    var header_sizes = [];
-    $('.item-header').each(function() { header_sizes.push($(this).outerHeight()) });
-    var tallest_header = Math.max.apply( null, header_sizes );
 
-    $('.item-header').each(function(){
+    /* getting viewport width */
+    var responsive_viewport = $(window).width();
+    
 
-        var item_width = $(this).parent().width();
 
-        $(this).css('height', tallest_header);
-        $(this).css('width', item_width); 
+    if (responsive_viewport < 768) {
+        
+      var $container = $('#container');
+      $container.isotope({
+        animationEngine: 'jquery',
+        resizable: false, 
+        masonry: { columnWidth: $container.width() / 2 }
+      });
 
-    });
+      // update columnWidth on window resize
+      $(window).smartresize(function(){
+        $container.isotope({
+          // update columnWidth to a percentage of container width
+          masonry: { columnWidth: $container.width() / 2 }
+        });
+      });
+
+    } /* end smallest screen */
+    
+
+
+    if (responsive_viewport >= 768) {
+
+      var $container = $('#container');
+
+      $container.isotope({
+        animationEngine: 'jquery',
+        resizable: false, 
+        masonry: { columnWidth: $container.width() / 3 }
+      });
+
+
+      $(window).smartresize(function(){
+        $container.isotope({
+          masonry: { columnWidth: $container.width() / 3 }
+        });
+      });
+
+   }
 }); /* end of as page load scripts */
