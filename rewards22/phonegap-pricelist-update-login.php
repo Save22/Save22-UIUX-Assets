0c64d5b4d3ef14ea5cc5d69ad55567014e40f12c
Camille Zapata <melonlime@gmail.com>
2012-12-26 17:20:29 +0800

    woohoo login page.

---
+++ rewards22/phonegap-pricelist-update-login.php
@@ -0,0 +1,33 @@
+<?php require_once('header.php'); ?>
+
+<div id="container">
+   <div id="wrap" class="wrap">
+
+      <div id="content" class="content">
+        <h1 class="page-header">Price List Update Login</h1>
+
+        <form>
+          <div class="row">
+            <div class="two mobile-one columns">
+              <label class="right inline">username:</label>
+            </div>
+            <div class="ten mobile-three columns">
+              <input type="text" placeholder="username" class="four" />
+            </div>
+          </div>
+          <div class="row">
+            <div class="two mobile-one columns">
+              <label class="right inline">password</label>
+            </div>
+            <div class="ten mobile-three columns">
+              <input type="password" placeholder="password" class="four" />
+            </div>
+          </div>
+        </form>
+      </div>
+
+   </div>
+</div>
+
+
+<?php require_once('footer.php'); ?>
