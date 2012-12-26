9c4f59d98e8256e05593f0897ca1f385d5630d65
Camille Zapata <melonlime@gmail.com>
2012-12-26 14:02:37 +0800

    Price Update Tasks page added.

---
+++ rewards22/elements/pricelist-update-task-table.php
@@ -0,0 +1,116 @@
+        <header>
+          <h1 class="page-header">Price Update Tasks</h1>
+        </header>
+
+         <table id="pricelist-update-tasks" class="twelve data">
+           <thead>
+             <tr>
+               <th class="th-index"></th>
+               <th class="th-prodname">Product Name</th>
+               <th class="th-lastprice">Last Known Price</th>
+               <th class="th-todayprice">Price Today</th>
+             </tr>
+           </thead>
+           <tbody>
+             <tr>
+              <td class="td-index">
+                1
+              </td>
+              <td class="td-prodname">
+                Galaxy S III
+              </td>
+              <td class="td-lastprice">
+                $948
+              </td>
+              <td class="td-todayprice">
+                <a href="#" class="button radius button-same">same</a>
+                <a href="#" class="button radius button-new">new price</a>
+                <a href="#" class="button radius button-unknown">?</a>
+              </td>
+             </tr>
+             <tr>
+              <td class="td-index">
+                2
+              </td>
+              <td class="td-prodname">
+                Galaxy S III
+              </td>
+              <td class="td-lastprice">
+                $948
+              </td>
+              <td class="td-todayprice">
+                <a href="#" class="button radius button-same">same</a>
+                <a href="#" class="button radius button-new">new price</a>
+                <a href="#" class="button radius button-unknown">?</a>
+              </td>
+             </tr>
+             <tr>
+              <td class="td-index">
+                3
+              </td>
+              <td class="td-prodname">
+                Galaxy S III
+              </td>
+              <td class="td-lastprice">
+                $948
+              </td>
+              <td class="td-todayprice">
+                <a href="#" class="button radius button-same">same</a>
+                <a href="#" class="button radius button-new">new price</a>
+                <a href="#" class="button radius button-unknown">?</a>
+              </td>
+             </tr>
+             <tr>
+              <td class="td-index">
+                4
+              </td>
+              <td class="td-prodname">
+                Galaxy S III
+              </td>
+              <td class="td-lastprice">
+                $948
+              </td>
+              <td class="td-todayprice">
+                <a href="#" class="button radius button-same">same</a>
+                <a href="#" class="button radius button-new">new price</a>
+                <a href="#" class="button radius button-unknown">?</a>
+              </td>
+             </tr>
+             <tr>
+              <td class="td-index">
+                5
+              </td>
+              <td class="td-prodname">
+                Galaxy S III
+              </td>
+              <td class="td-lastprice">
+                $948
+              </td>
+              <td class="td-todayprice">
+                <a href="#" class="button radius button-same">same</a>
+                <a href="#" class="button radius button-new">new price</a>
+                <a href="#" class="button radius button-unknown">?</a>
+              </td>
+             </tr>
+           </tbody>
+         </table>
+
+
+
+
+        <div class="group">
+          <ul class="pagination">
+            <li class="arrow unavailable"><a href="">&laquo;</a></li>
+            <li class="current"><a href="">1</a></li>
+            <li><a href="">2</a></li>
+            <li><a href="">3</a></li>
+            <li><a href="">4</a></li>
+            <li class="unavailable"><a href="">&hellip;</a></li>
+            <li><a href="">12</a></li>
+            <li><a href="">13</a></li>
+            <li class="arrow"><a href="">&raquo;</a></li>
+          </ul>
+        </div>
+        
+
+        
