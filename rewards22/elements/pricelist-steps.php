f19b62b7f8915eb5c78b155039a0c5e0dd6d1f89
Camille Zapata <melonlime@gmail.com>
2012-12-28 17:21:27 +0800

    updated initial pricesheet processing steps

--- rewards22/elements/pricelist-steps.php
+++ rewards22/elements/pricelist-steps.php
@@ -14,7 +14,7 @@
       <p>Search for existing SKUs using one of the methods below to avoid duplicate entries. Be very careful with selecting only exact matches. </p>
 
 
-      <div class="group">
+      <div class="group find-existing">
         <div class="half-page">
           <input type="text" class="search" placeholder="Search Model Number">
             <p>no spaces.</p>
@@ -43,15 +43,15 @@
     <h3>SKU Search Result</h3>
     <p>select an existing SKU below if you can find an exact match for the product you're looking at. If not, create a new SKU.</p>
 
-      <div class="group">
+      <div class="group search-results">
 
-        <label for="radio4"><input name="radio2" type="radio" id="radio4"> Radio Button 1<br>
+        <label for="radio4"><input name="radio2" type="radio" id="radio4"> Product Title 1<br>
           <span class="scraped-url">http://scrapedurl.com</span>
         </label>
-        <label for="radio5"><input name="radio2" CHECKED type="radio" id="radio5"> Radio Button 2<br>
+        <label for="radio5"><input name="radio2" CHECKED type="radio" id="radio5"> Product Title 2<br>
           <span class="scraped-url">http://scrapedurl.com</span>
         </label>
-        <label for="radio6"><input name="radio2" type="radio" id="radio6"> Radio Button 3<br>
+        <label for="radio6"><input name="radio2" type="radio" id="radio6"> Product Title 3<br>
           <span class="scraped-url">http://scrapedurl.com</span>
         </label>
 
@@ -65,13 +65,133 @@
       </div>
 
    </div>
-   <div class="step">
-   </div>
-   <div class="step">
-   </div>
-   <div class="step">
-   </div>
-   <div class="step">
+
+  <div class="step">
+    <!-- if SKU not found -->
+    <div class="alert-box">
+      Your SKU is new.
+      <a href="" class="close">&times;</a>
+    </div>
+
+    <h3>2. You found a new SKU. Add it: </h3>
+
+      <div class="row">
+        <div class="two mobile-one columns">
+          <label class="right inline">Title</label>
+        </div>
+        <div class="ten mobile-three columns">
+          <input type="text" name="sku_title" placeholder="Title" class="eight" />
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="two mobile-one columns">
+          <label class="right inline">Brand</label>
+        </div>
+        <div class="ten mobile-three columns">
+          <input type="text" name="sku_brand" placeholder="Brand" class="eight" />
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="two mobile-one columns">
+          <label class="right inline">Description</label>
+        </div>
+        <div class="ten mobile-three columns">
+          <textarea name="sku_desc" class="eight"></textarea>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="two mobile-one columns">
+          <label class="right inline">Category</label>
+        </div>
+        <div class="ten mobile-three columns">
+          <input type="text" name="sku_category" placeholder="Category" class="eight" />
+          category is a blah blah blah
+          <br><br>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="two mobile-one columns">
+          <label class="right inline">Image Link</label>
+        </div>
+        <div class="ten mobile-three columns">
+          <input type="text" name="sku_image" placeholder="http://" class="eight" />
+        </div>
+      </div>
+
+
+  </div>
+
+  <div class="step">
+    <div class="alert-box success">
+    <!-- if SKU found -->
+      <h3>Model: A1428</h3>
+      <a href="" class="close">&times;</a>
+    </div>
+    
+    <h3>2/3. Add Buying Option</h3>
+    <!-- Right aligned and inline labels -->
+      <div class="row">
+        <div class="two mobile-one columns">
+          <label class="right inline">Price</label>
+        </div>
+        <div class="ten mobile-three columns">
+          <input type="text" placeholder="Price" class="eight" />
+        </div>
+      </div>
+      <div class="row">
+        <div class="two mobile-one columns">
+          <label class="right inline">Price List Name</label>
+        </div>
+        <div class="ten mobile-three columns">
+          <select id="priceListName">
+              <option SELECTED>Choose price list name</option>
+              <option>a price list</option>
+              <option>b price list</option>
+              <option>c price list</option>
+              <option>d price list</option>
+              <option>e price list</option>
+              <option>f price list</option>
+              <option>g price list</option>
+              <option>h price list</option>
+              <option>i price list</option>
+          </select>
+
+        </div>
+      </div>
+      <div class="row">
+        <div class="two mobile-one columns">
+          <label class="right inline">Store</label>
+        </div>
+        <div class="ten mobile-three columns">
+          <select id="priceListName">
+              <option SELECTED>Choose a store</option>
+              <option>a store </option>
+              <option>b store </option>
+              <option>c store </option>
+              <option>d store </option>
+              <option>e store </option>
+          </select>
+        </div>
+      </div>
+
+    <h3>3/4. Need help here?</h3>
+
+      <div class="row">
+        <div class="two mobile-one columns">
+          <label class="right inline">Should this entry be rechecked?</label>
+        </div>
+        <div class="ten mobile-three columns checklist-radio">
+          <label for="recheck"><input name="recheck" type="radio" id="recheck"> 
+            <strong>Yes</strong>, please recheck this</label>
+          <label for="norecheck"><input name="recheck" CHECKED type="radio" id="norecheck"> 
+            <strong>No need</strong> to recheck this</label>
+        </div>
+      </div>
+
    </div>
   </div><!-- pricesheet-processing-steps -->
 
