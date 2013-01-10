5e7551976daad15d645d20551153e4b27729c672
Camille Zapata <camillezapata@gmail.com>
2013-01-10 09:58:51 +0800

    changes made to Store Page

---
+++ v2/serp-similarresults.php
@@ -0,0 +1,29 @@
+<?php require_once('header.php'); ?>
+<?php require_once('widgets/notice-shopsmarter.php'); ?>
+
+<div id="content" class="search">
+    <div class="row">
+
+      <div class="twelve columns">
+        <!-- CRUMBS NAVIGATION: so people know where they are -->
+        <nav id="crumbs">
+          <a href="#" class="home">Home</a> &rsaquo; 
+          <a href="#" class="current-page">Search results for: "nintendo ds xl"</a>
+        </nav>
+
+        <h2 class="single-header page-header">Search results for: "nintendo ds xl"</h2>
+        
+        <p><span class="results-copy">We could not find <strong>"Nintendo DS XL"</strong></span> <br />
+          Did you mean <em><a href="http://www.save22.com.ph/p/search/?q=ds">ds</a> or <a href="http://www.save22.com.ph/p/search/?q=nintendo">nintendo</a></em>?</p>
+
+        <!-- IF: Show similar results instead -->
+        <p><strong>There are similar items on the site. Maybe these would interest you?</strong></p>
+
+        // show similar results: <a href="http://www.save22.com.ph/p/search/?q=ds">For example, results from "DS"</a>
+      </div><!-- ninecol group -->
+
+      
+    </div><!-- row -->
+</div><!-- content -->
+
+<?php require_once('footer.php'); ?>
