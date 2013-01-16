4f2fc5f8de35a58fe7f59552a56871cf119ee343
Camille Zapata <melonlime@gmail.com>
2013-01-16 11:55:45 +0800

    changed the JS links to rewards22 footer because one of the js codes (forms) is causing an error

--- rewards22/footer.php
+++ rewards22/footer.php
@@ -24,8 +24,26 @@
 
   <!-- Included JS Files (Compressed) -->
   <script src="js/jquery.js"></script>
-  <script src="js/foundation.min.js"></script>
+  <!-- <script src="js/foundation.min.js"></script> -->
+
+  <!-- individual JS files for debugging. forms.js causing an error -->
+  <script src="javascripts/jquery.js"></script>
+  <script src="javascripts/jquery.foundation.mediaQueryToggle.js"></script>
+  <script src="javascripts/jquery.foundation.reveal.js"></script>
+  <script src="javascripts/jquery.foundation.orbit.js"></script>
+  <script src="javascripts/jquery.foundation.navigation.js"></script>
+  <script src="javascripts/jquery.foundation.buttons.js"></script>
+  <script src="javascripts/jquery.foundation.tabs.js"></script>
+  <script src="javascripts/jquery.foundation.tooltips.js"></script>
+  <script src="javascripts/jquery.foundation.accordion.js"></script>
+  <script src="javascripts/jquery.placeholder.js"></script>
+  <script src="javascripts/jquery.foundation.alerts.js"></script>
+  <script src="javascripts/jquery.foundation.topbar.js"></script>
+  <script src="javascripts/jquery.foundation.joyride.js"></script>
+  <script src="javascripts/jquery.foundation.clearing.js"></script>
+  <script src="javascripts/jquery.foundation.magellan.js"></script>
   
+
   <!-- Initialize JS Plugins -->
   <script src="js/app.js"></script>
   <script src="js/tablednd.js"></script>
