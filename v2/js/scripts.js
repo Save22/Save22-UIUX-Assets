9f7501a8f0050564760deecd6201ffadf25aea87
Camille Zapata <camillezapata@gmail.com>
2013-01-30 14:22:36 +0800

    minor fix for header in mobile view, v1 and v2

--- v2/js/scripts.js
+++ v2/js/scripts.js
@@ -49,7 +49,7 @@ jQuery(document).ready(function($) {
         
         
         /** TOPMOST HEADER **/
-        
+        /* 
             var div = $('#main-header');
             var start = $(div).offset().top;
             var div_height = div.height() + $('#smart-shopper').outerHeight(true);
@@ -65,7 +65,32 @@ jQuery(document).ready(function($) {
                     $(div).removeClass('floating-header');
                 }
             });
+        */
+
+        // grab the initial top offset of the navigation 
+        var sticky_navigation_offset_top = $('#main-header').offset().top;
         
+        // our function that decides weather the navigation bar should have "fixed" css position or not.
+        var sticky_navigation = function(){
+            var scroll_top = $(window).scrollTop(); // our current vertical position from the top
+            
+            // if we've scrolled more than the navigation, change its position to fixed to stick to top, otherwise change it back to relative
+            if (scroll_top > sticky_navigation_offset_top) { 
+                $('#main-header').css({ 'position': 'fixed', 'top':0, 'left':0 }).addClass('floating-header');
+            } else {
+                $('#main-header').css({ 'position': 'relative' }).removeClass('floating-header'); 
+            }   
+        };
+        
+        // run our function on load
+        sticky_navigation();
+        
+        // and run it again every time you scroll
+        $(window).scroll(function() {
+             sticky_navigation();
+        });
+        
+
 
     /* store header nav */ 
 
