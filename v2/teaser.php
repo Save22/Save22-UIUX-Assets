3afec25ad292bf62bedea1754dc0d3ffc8b8a049
Camille Zapata <camillezapata@gmail.com>
2013-03-20 10:56:44 +0800

    teaser template

---
+++ v2/teaser.php
@@ -0,0 +1,19 @@
+<?php require_once('header.php'); ?>
+   
+<div id="content">
+
+<div class="segment">
+   <div class="row">
+      <div class="twelve columns">
+
+        <iframe src="http://eepurl.com/rnp71" seamless width="100%" height="600"></iframe>
+
+      </div>
+   </div><!-- row -->
+</div><!-- about -->
+
+
+</div><!-- content -->
+
+
+<?php require_once('footer.php'); ?>
