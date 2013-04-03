c6c0d6c3c1982242be915d0a7f51d889dce4a268
Camille Zapata <camillezapata@gmail.com>
2013-04-03 09:17:38 +0800

    created a better sample of the image upload

---
+++ catalogue-data-collection/plugins/jQuery-File-Upload-master/header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width" />
+  <title>Save22 - Catalogue Acquisition</title>
+
+  <link rel="stylesheet" href="css/normalize.css" />
+  
+    
+  <!-- Bootstrap CSS Toolkit styles -->
+  <link rel="stylesheet" href="http://blueimp.github.com/cdn/css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/jquery.fileupload-ui.css">
+  <noscript><link rel="stylesheet" href="css/jquery.fileupload-ui-noscript.css"></noscript>
+
+  <link rel="stylesheet" href="css/foundation.css" />
+  <link rel="stylesheet" href="css/pikaday.css">
+  
+  <script src="js/vendor/custom.modernizr.js"></script>
+
+</head>
+<body>
+<nav class="top-bar">
+  <ul class="title-area">
+    <!-- Title Area -->
+    <li class="name">
+      <h1><a href="#">Save22 - Catalogue Acquisition</a></h1>
+    </li>
+    <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
+    <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+  </ul>
+
+  <section class="top-bar-section">
+    <!-- Left Nav Section -->
+    <ul class="right">
+      <li class="divider"></li>
+      <li><a href="#">ADMIN</a></li>
+      <li class="divider"></li>
+      <li><a href="#">List Catalogues</a></li>
+      <li class="divider"></li>
+      <li class="active"><a href="#">Add a Catalogue</a></li>
+      <li class="divider"></li>
+      <li><a href="#">Logout</a></li>
+      <li class="divider"></li>
+    </ul>
+
+
+  </section>
+</nav>
