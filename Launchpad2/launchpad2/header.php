bdb797e437afeae80f2874283aad0f350c88f89e
Camille Zapata <camillezapata@gmail.com>
2013-04-08 16:26:24 +0800

    initial assets for Launchpad 2.0 - work in progress

---
+++ Launchpad2/launchpad2/header.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<!--[if IE 8]><html class="no-js lt-ie9" lang="en" > <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en" > <!--<![endif]-->
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width" />
+  <title>Save22 | Launchpad 2.0</title>
+
+  <link rel="stylesheet" href="css/normalize.css" />
+  <link rel="stylesheet" href="css/app.css" />
+  <link rel="stylesheet" href="css/styles.css" />
+  
+
+  <script src="js/vendor/custom.modernizr.js"></script>
+
+
+  <header>
+
+    <nav class="top-bar">
+      <ul class="title-area">
+        <!-- Title Area -->
+        <li class="name">
+          <h1><a href="#">Save22.com.ph</a></h1>
+        </li>
+        <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
+        <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+      </ul>
+
+      <section class="top-bar-section">
+        <!-- Left Nav Section -->
+        <ul class="left">
+          <li class="divider"></li>
+          <li class="has-dropdown"><a href="#">Categories</a>
+            <ul class="dropdown">
+              <li><a href="#">SALES</a></li>
+              <li><a href="#">Babies &amp; Toddlers</a></li>
+              <li><a href="#">Cars</a></li>
+              <li><a href="#">Clothes and Fashion</a></li>
+              <li><a href="#">Electronics</a></li>
+              <li><a href="#">Home and Furniture</a></li>
+              <li><a href="#">Gardening</a></li>
+              <li><a href="#">Shoes</a></li>
+              <li><a href="#">Sports</a></li>
+              <li><a href="#">Supermarkets</a></li>
+              <li class="divider"></li>
+              <li><a href="#">See all &rarr;</a></li>
+            </ul>
+          </li>
+          <li class="divider"></li>
+          <li class="has-form">
+            <form>
+              <div class="row collapse">
+                <div class="small-4 columns">
+                  <label class="inline" for="search">You're at</label>
+                </div>
+                <div class="small-8 columns">
+                  <input name="search" id="search" type="text" placeholder="Ortigas" value="Ortigas">
+                </div>
+              </div>
+            </form>
+          </li>
+        </ul>
+
+        <!-- Right Nav Section -->
+        <ul class="right">
+          <li class="divider"></li>
+          <li class="has-form">
+            <form>
+              <div class="row collapse">
+                <div class="small-8 columns">
+                  <input type="search" placeholder="Search for promos, items, prices, or stores">
+                </div>
+                <div class="small-4 columns">
+                  <a href="#" class="alert button">Search</a>
+                </div>
+              </div>
+            </form>
+          </li>
+        </ul>
+      </section>
+    </nav>
+
+  </header>
+
+</head>
+<body>
---
+++ Launchpad2/launchpad2/main-home.php
@@ -0,0 +1,16 @@
+<?php include('header.php'); ?>
+  
+  <div class="home-intro">
+    <strong>Discover the best deals in town.</strong>
+  </div>
+
+  <div class="row">
+    <div class="small-12 large-4 columns">
+
+    </div>
+    <div class="small-12 large-8 columns">
+      <iframe width="100%" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Podium,+Ortigas&amp;aq=&amp;sll=37.0625,-95.677068&amp;sspn=49.978077,107.226563&amp;ie=UTF8&amp;hq=Podium,+Ortigas&amp;hnear=&amp;radius=15000&amp;ll=14.585837,121.059294&amp;spn=0.007517,0.013089&amp;t=m&amp;z=14&amp;iwloc=A&amp;cid=9322337523818707896&amp;output=embed"></iframe>
+    </div>
+  </div>
+
+<?php include('footer.php'); ?>
