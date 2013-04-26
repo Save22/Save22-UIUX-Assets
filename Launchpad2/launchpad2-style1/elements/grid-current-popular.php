9cee70b0f9f387f4cf6b68cc1cadcca2fe2bb572
Camille Zapata <camillezapata@gmail.com>
2013-04-26 20:32:37 +0800

    added Promos-stores page, LAUN-74

---
+++ Launchpad2/launchpad2-style1/elements/grid-current-popular.php
@@ -0,0 +1,243 @@
+
+        <ul class="small-block-grid-2 large-block-grid-5 featured-grid">
+          <li class="item">
+            <div class="cat-thumb">
+              <a href="#">
+                <div class="cat-info">
+                  <h4>Catalogue Title</h4>
+                </div>
+                <div class="thumb">
+                  <img src="img/catsample1.jpg" alt="">
+                </div>
+              </a>
+            </div>
+          </li>
+          <li class="item">
+            <div class="cat-thumb">
+              <a href="#">
+                <div class="cat-info">
+                  <h4>Catalogue Title</h4>
+                </div>
+                <div class="thumb">
+                  <img src="img/catsample4.jpg" alt="">
+                </div>
+              </a>
+            </div>
+          </li>
+          <li class="item">
+            <div class="cat-thumb">
+              <a href="#">
+                <div class="cat-info">
+                  <h4>Catalogue Title</h4>
+                </div>
+                <div class="thumb">
+                  <img src="img/catsample2.jpg" alt="">
+                </div>
+              </a>
+            </div>
+          </li>
+          <li class="item">
+            <div class="cat-thumb">
+              <a href="#">
+                <div class="cat-info">
+                  <h4>Catalogue Title</h4>
+                </div>
+                <div class="thumb">
+                  <img src="img/catsample3.jpg" alt="">
+                </div>
+              </a>
+            </div>
+          </li>
+          <li class="item">
+            <div class="cat-thumb">
+              <a href="#">
+                <div class="cat-info">
+                  <h4>Catalogue Title</h4>
+                </div>
+                <div class="thumb">
+                  <img src="img/catsample1.jpg" alt="">
+                </div>
+              </a>
+            </div>
+          </li>
+          <li class="item">
+            <div class="cat-thumb">
+              <a href="#">
+                <div class="cat-info">
+                  <h4>Catalogue Title</h4>
+                </div>
+                <div class="thumb">
+                  <img src="img/catsample4.jpg" alt="">
+                </div>
+              </a>
+            </div>
+          </li>
+          <li class="item">
+            <div class="cat-thumb">
+              <a href="#">
+                <div class="cat-info">
+                  <h4>Catalogue Title</h4>
+                </div>
+                <div class="thumb">
+                  <img src="img/catsample2.jpg" alt="">
+                </div>
+              </a>
+            </div>
+          </li>
+          <li class="item">
+            <div class="cat-thumb">
+              <a href="#">
+                <div class="cat-info">
+                  <h4>Catalogue Title</h4>
+                </div>
+                <div class="thumb">
+                  <img src="img/catsample3.jpg" alt="">
+                </div>
+              </a>
+            </div>
+          </li>
+          <li class="item">
+            <div class="cat-thumb">
+              <a href="#">
+                <div class="cat-info">
+                  <h4>Catalogue Title</h4>
+                </div>
+                <div class="thumb">
+                  <img src="img/catsample2.jpg" alt="">
+                </div>
+              </a>
+            </div>
+          </li>
+          <li class="item">
+            <div class="cat-thumb">
+              <a href="#">
+                <div class="cat-info">
+                  <h4>Catalogue Title</h4>
+                </div>
+                <div class="thumb">
+                  <img src="img/catsample3.jpg" alt="">
+                </div>
+              </a>
+            </div>
+          </li>
+          <li class="item">
+            <div class="cat-thumb">
+              <a href="#">
+                <div class="cat-info">
+                  <h4>Catalogue Title</h4>
+                </div>
+                <div class="thumb">
+                  <img src="img/catsample1.jpg" alt="">
+                </div>
+              </a>
+            </div>
+          </li>
+          <li class="item">
+            <div class="cat-thumb">
+              <a href="#">
+                <div class="cat-info">
+                  <h4>Catalogue Title</h4>
+                </div>
+                <div class="thumb">
+                  <img src="img/catsample4.jpg" alt="">
+                </div>
+              </a>
+            </div>
+          </li>
+          <li class="item">
+            <div class="cat-thumb">
+              <a href="#">
+                <div class="cat-info">
+                  <h4>Catalogue Title</h4>
+                </div>
+                <div class="thumb">
+                  <img src="img/catsample2.jpg" alt="">
+                </div>
+              </a>
+            </div>
+          </li>
+          <li class="item">
+            <div class="cat-thumb">
+              <a href="#">
+                <div class="cat-info">
+                  <h4>Catalogue Title</h4>
+                </div>
+                <div class="thumb">
+                  <img src="img/catsample3.jpg" alt="">
+                </div>
+              </a>
+            </div>
+          </li>
+          <li class="item">
+            <div class="cat-thumb">
+              <a href="#">
+                <div class="cat-info">
+                  <h4>Catalogue Title</h4>
+                </div>
+                <div class="thumb">
+                  <img src="img/catsample1.jpg" alt="">
+                </div>
+              </a>
+            </div>
+          </li>
+          <li class="item">
+            <div class="cat-thumb">
+              <a href="#">
+                <div class="cat-info">
+                  <h4>Catalogue Title</h4>
+                </div>
+                <div class="thumb">
+                  <img src="img/catsample4.jpg" alt="">
+                </div>
+              </a>
+            </div>
+          </li>
+          <li class="item">
+            <div class="cat-thumb">
+              <a href="#">
+                <div class="cat-info">
+                  <h4>Catalogue Title</h4>
+                </div>
+                <div class="thumb">
+                  <img src="img/catsample2.jpg" alt="">
+                </div>
+              </a>
+            </div>
+          </li>
+          <li class="item">
+            <div class="cat-thumb">
+              <a href="#">
+                <div class="cat-info">
+                  <h4>Catalogue Title</h4>
+                </div>
+                <div class="thumb">
+                  <img src="img/catsample3.jpg" alt="">
+                </div>
+              </a>
+            </div>
+          </li>
+          <li class="item">
+            <div class="cat-thumb">
+              <a href="#">
+                <div class="cat-info">
+                  <h4>Catalogue Title</h4>
+                </div>
+                <div class="thumb">
+                  <img src="img/catsample2.jpg" alt="">
+                </div>
+              </a>
+            </div>
+          </li>
+          <li class="item">
+            <div class="cat-thumb">
+              <a href="#">
+                <div class="cat-info">
+                  <h4>Catalogue Title</h4>
+                </div>
+                <div class="thumb">
+                  <img src="img/catsample3.jpg" alt="">
+                </div>
+              </a>
+            </div>
+          </li>
+        </ul>
---
+++ Launchpad2/launchpad2-style1/promos-stores1.php
@@ -0,0 +1,27 @@
+<?php include('header.php'); ?>
+  
+  <div class="row main-row">
+    <ul class="breadcrumbs">
+      <li><a href="#">Save22 Ortigas</a></li>
+      <li class="current"><a href="#">Current Offers</a></li>
+    </ul>
+  </div>
+  
+  <section class="home-preview">
+    <div class="row main-row">
+      <div class="small-12 large-9 columns">
+        <h2>Current Popular Promos <br>from Shops in <span class="location">Pasig City</span> within 25km</h2>
+        <h3 class="sub-header">Brochures</h3>
+          <?php include('elements/grid-current-popular.php'); ?>
+          <?php include('elements/pagination.php'); ?>
+      </div>
+      <div class="small-12 large-3 columns">
+        
+        <?php include('elements/sidebar-categories.php'); ?>
+
+      </div>
+    </div>
+  </section>
+
+
+<?php include('footer.php'); ?>
