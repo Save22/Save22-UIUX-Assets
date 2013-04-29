b5db7e19988499b211dbe12462315b3f1242f913
Camille Zapata <camillezapata@gmail.com>
2013-04-29 18:04:28 +0800

    modified main-home4 a little

--- Launchpad2/launchpad2-style1/main-home4.php
+++ Launchpad2/launchpad2-style1/main-home4.php
@@ -2,127 +2,7 @@
   
   <div class="row home-featured main-row">
     <div class="small-12 large-9 columns main-content">
-
-      <section class="location-ads">
-        <h2 class="main-header">Promos near you</h2>
-        <div class="map">
-          <iframe width="100%" height="250" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Podium,+Ortigas&amp;aq=&amp;sll=37.0625,-95.677068&amp;sspn=49.978077,107.226563&amp;ie=UTF8&amp;hq=Podium,+Ortigas&amp;hnear=&amp;radius=15000&amp;ll=14.585837,121.059294&amp;spn=0.007517,0.013089&amp;t=m&amp;z=14&amp;iwloc=A&amp;cid=9322337523818707896&amp;output=embed"></iframe>
-        </div>
-        <div class="promos-near">
-          <ul id="featured1" data-orbit>
-            <li data-orbit-slide="headline-1">
-              <div class="row featured-set">
-                <div class="small-6 columns">
-                  <div class="featured-promo-thumb">
-                    <a href="#">
-                      <img src="img/catsample6.jpg" alt="">
-                    </a>
-                  </div><!-- promo -->
-                </div>
-                <div class="small-6 columns">
-                  <h3>Cold Storage</h3>
-                  <span class="nearest"><a href="#">Nearest Branch: 2.5km away</a></a><br>
-                  <span class="valid-date">Valid until April 12, 2013</span><br><br>
-
-                  <a href="#" class="button">View Catalogue</a>
-                </div>
-              </div><!-- row -->
-            </li>
-            <li data-orbit-slide="headline-2">
-              <div class="row featured-set">
-                <div class="small-6 columns">
-                  <div class="featured-promo-thumb">
-                    <a href="#">
-                      <img src="img/catsample5.jpg" alt="">
-                    </a>
-                  </div><!-- promo -->
-                </div>
-                <div class="small-6 columns">
-                  <h3>Cold Storage</h3>
-                  <span class="nearest"><a href="#">Nearest Branch: 2.5km away</a></a><br>
-                  <span class="valid-date">Valid until April 12, 2013</span><br><br>
-
-                  <a href="#" class="button">View Catalogue</a>
-                </div>
-              </div><!-- row -->
-            </li>
-            <li data-orbit-slide="headline-3">
-              <div class="row featured-set">
-                <div class="small-6 columns">
-                  <div class="featured-promo-thumb">
-                    <a href="#">
-                      <img src="img/catsample4.jpg" alt="">
-                    </a>
-                  </div><!-- promo -->
-                </div>
-                <div class="small-6 columns">
-                  <h3>Cold Storage</h3>
-                  <span class="nearest"><a href="#">Nearest Branch: 2.5km away</a></a><br>
-                  <span class="valid-date">Valid until April 12, 2013</span><br><br>
-
-                  <a href="#" class="button">View Catalogue</a>
-                </div>
-              </div><!-- row -->
-            </li>
-            <li data-orbit-slide="headline-4">
-              <div class="row featured-set">
-                <div class="small-6 columns">
-                  <div class="featured-promo-thumb">
-                    <a href="#">
-                      <img src="img/catsample6.jpg" alt="">
-                    </a>
-                  </div><!-- promo -->
-                </div>
-                <div class="small-6 columns">
-                  <h3>Cold Storage</h3>
-                  <span class="nearest"><a href="#">Nearest Branch: 2.5km away</a></a><br>
-                  <span class="valid-date">Valid until April 12, 2013</span><br><br>
-
-                  <a href="#" class="button">View Catalogue</a>
-                </div>
-              </div><!-- row -->
-            </li>
-            <li data-orbit-slide="headline-5">
-              <div class="row featured-set">
-                <div class="small-6 columns">
-                  <div class="featured-promo-thumb">
-                    <a href="#">
-                      <img src="img/catsample5.jpg" alt="">
-                    </a>
-                  </div><!-- promo -->
-                </div>
-                <div class="small-6 columns">
-                  <h3>Cold Storage</h3>
-                  <span class="nearest"><a href="#">Nearest Branch: 2.5km away</a></a><br>
-                  <span class="valid-date">Valid until April 12, 2013</span><br><br>
-
-                  <a href="#" class="button">View Catalogue</a>
-                </div>
-              </div><!-- row -->
-            </li>
-            <li data-orbit-slide="headline-6">
-              <div class="row featured-set">
-                <div class="small-6 columns">
-                  <div class="featured-promo-thumb">
-                    <a href="#">
-                      <img src="img/catsample4.jpg" alt="">
-                    </a>
-                  </div><!-- promo -->
-                </div>
-                <div class="small-6 columns">
-                  <h3>Cold Storage</h3>
-                  <span class="nearest"><a href="#">Nearest Branch: 2.5km away</a></a><br>
-                  <span class="valid-date">Valid until April 12, 2013</span><br><br>
-
-                  <a href="#" class="button">View Catalogue</a>
-                </div>
-              </div><!-- row -->
-            </li>
-          </ul>
-
-        </div>
-      </section>
-
+      <?php include('elements/home-map.php'); ?>
     </div>
 
     <div class="small-12 large-3 columns sidebar">
@@ -137,6 +17,8 @@
 
         <h3 class="sub-header">Brochures</h3>
           <?php include('elements/grid-featured.php'); ?>
+
+          <a href="#category-link" class="button-minor home-more">view more</a>
 
         <h3 class="sub-header">Brands</h3>
           <?php include('elements/carousel-brands.php'); ?>
