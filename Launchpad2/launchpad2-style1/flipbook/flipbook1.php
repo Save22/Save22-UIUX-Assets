2247e23722fd8bd0f1732444f2f140d30b33c17b
Lope Emano <lopegwapo@gmail.com>
2013-05-02 19:30:28 +0800

    configured everything as per example

--- Launchpad2/launchpad2-style1/flipbook/flipbook1.php
+++ Launchpad2/launchpad2-style1/flipbook/flipbook1.php
@@ -15,8 +15,9 @@
   <script src="../js/vendor/custom.modernizr.js"></script>
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
   <script src="../js/jquery.caroufredsel.min.js"></script>
-  <script type="text/javascript" src="../js/turn.min.js"></script>
-  <script type="text/javascript" src="../js/turn.html4.min.js"></script>
+  <script type="text/javascript" src="../js/turn.js"></script>
+  <script type="text/javascript" src="../js/zoom.js"></script>
+  <!-- <script type="text/javascript" src="../js/turn.html4.min.js"></script> -->
   <script type="text/javascript" src="../js/jquery.mousewheel.min.js"></script>
   <script type="text/javascript" src="../js/hash.js"></script>
   <script type="text/javascript" src="../js/magazine.js"></script>
@@ -98,9 +99,9 @@
                   <img src="pages/5.jpg" alt="">
                 </div>
 				<!-- Next button -->
-				<div ignore="1" class="next-button previous-button-hover"></div>
+				<div ignore="1" class="next-button"></div>
 				<!-- Previous button -->
-				<div ignore="1" class="previous-button previous-button-hover"></div>
+				<div ignore="1" class="previous-button"></div>
               </div>
         </div><!-- container -->
 
@@ -174,52 +175,194 @@
 var turner;
     $(document).ready(function() {
 		turner = $('.magazine').turn({
-				// Magazine width
-				width: 720,
-				// Magazine height
-				height: 400,
-				// Elevation will move the peeling corner this number of pixels by default
-				elevation: 50,
-				// Enables gradients
-				gradients: true,
-				// Auto center this flipbook
-				autoCenter: true,
-				// The number of pages
-				pages: 12,
-				// Events
-				when: {
-				turning: function(event, page, view) {
-					var book = $(this),
-					currentPage = book.turn('page'),
-					pages = book.turn('pages');
-					// Update the current URI
-					Hash.go('page/' + page).update();
-					// Show and hide navigation buttons
-					disableControls(page);
-					$('.thumbnails .page-'+currentPage).
-						parent().
-						removeClass('current');
-					$('.thumbnails .page-'+page).
-						parent().
-						addClass('current');
-				},
-				turned: function(event, page, view) {
-					disableControls(page);
-					setCurrentPage(page);
-					var total_pages = $('.magazine >div').length;
-					if (total_pages <= page + 2){
-						//alert(page + 'yes' + total_pages);
-						//addPage(page + 1, $('.magazine'));
-					}
-				},
-				missing: function (event, pages) {
-					console.log(pages)
-					// Add pages that aren't in the magazine
-					for (var i = 0; i < pages.length; i++)
-						addPage(pages[i], $(this));
-				}
-			}
+		    // Magazine width
+		    width: 720,
+		    // Magazine height
+		    height: 400,
+		    // Elevation will move the peeling corner this number of pixels by default
+		    elevation: 50,
+		    // Hardware acceleration
+		    acceleration: !isChrome(),
+		    // Enables gradients
+		    gradients: true,
+		    // Auto center this flipbook
+		    autoCenter: true,
+		    // The number of pages
+		    pages: 12,
+		    // Events
+		    when: {
+		        turning: function(event, page, view) {
+		            var book = $(this),
+		            currentPage = book.turn('page'),
+		            pages = book.turn('pages');
+		            // Update the current URI
+		            Hash.go('page/' + page).update();
+		            // Show and hide navigation buttons
+		            disableControls(page);
+		            $('.thumbnails .page-' + currentPage).parent().removeClass('current');
+		            $('.thumbnails .page-' + page).parent().addClass('current');
+		        },
+		        turned: function(event, page, view) {
+		            disableControls(page);
+		            $(this).turn('center');
+		            if (page == 1) {
+		                $(this).turn('peel', 'br');
+		            }
+		        },
+		        missing: function(event, pages) {
+		        	console.log(pages);
+		            // Add pages that aren't in the magazine
+		            for (var i = 0; i < pages.length; i++){};
+		                //addPage(pages[i], $(this));
+		        }
+		    }
 		});
+		
+		// Zoom.js
+		$('.magazine-viewport').zoom({
+		    flipbook: $('.magazine'),
+		    max: function() {
+		        return largeMagazineWidth() / $('.magazine').width();
+		    },
+		    when: {
+		        doubleTap: function(event) {
+		            if ($(this).zoom('value') == 1) {
+		                $('.magazine').removeClass('animated').addClass('zoom-in');
+		                $(this).zoom('zoomIn', event);
+		            } else {
+		                $(this).zoom('zoomOut');
+		            }
+		        },
+		        resize: function(event, scale, page, pageElement) {
+		            if (scale == 1)
+		                loadSmallPage(page, pageElement);
+		            else
+		                loadLargePage(page, pageElement);
+		        },
+		        zoomIn: function() {
+		            $('.thumbnails').hide();
+		            $('.made').hide();
+		            $('.magazine').addClass('zoom-in');
+		            if (!window.escTip && !$.isTouch) {
+		                escTip = true;
+		                $('<div />', {
+		                    'class': 'esc'
+		                }).html('<div>Press ESC to exit</div>').appendTo($('body')).delay(2000).animate({
+		                    opacity: 0
+		                }, 500, function() {
+		                    $(this).remove();
+		                });
+		            }
+		        },
+		        zoomOut: function() {
+		            $('.esc').hide();
+		            $('.thumbnails').fadeIn();
+		            $('.made').fadeIn();
+		            setTimeout(function() {
+		                $('.magazine').addClass('animated').removeClass('zoom-in');
+		                resizeViewport();
+		            }, 0);
+		        },
+		
+		        swipeLeft: function() {
+		            $('.magazine').turn('next');
+		        },
+		
+		        swipeRight: function() {
+		            $('.magazine').turn('previous');
+		        }
+		    }
+		});
+		
+		// Using arrow keys to turn the page
+		$(document).keydown(function(e) {
+		
+		    var previous = 37,
+		    next = 39,
+		    esc = 27;
+		
+		    switch (e.keyCode) {
+		    case previous:
+		
+		        // left arrow
+		        $('.magazine').turn('previous');
+		        e.preventDefault();
+		
+		        break;
+		    case next:
+		
+		        //right arrow
+		        $('.magazine').turn('next');
+		        e.preventDefault();
+		
+		        break;
+		    case esc:
+		
+		        $('.magazine-viewport').zoom('zoomOut');
+		        e.preventDefault();
+		
+		        break;
+		    }
+		});
+		
+		// URIs - Format #/page/1 
+		Hash.on('^page\/([0-9]*)$', {
+		    yep: function(path, parts) {
+		        var page = parts[1];
+		
+		        if (page !== undefined) {
+		            if ($('.magazine').turn('is'))
+		                $('.magazine').turn('page', page);
+		        }
+		
+		    },
+		    nop: function(path) {
+		
+		        if ($('.magazine').turn('is'))
+		            $('.magazine').turn('page', 1);
+		    }
+		});
+		
+		$(window).resize(function() {
+		    resizeViewport();
+		}).bind('orientationchange', function() {
+		    resizeViewport();
+		});
+		
+		// Regions
+		if ($.isTouch) {
+		    $('.magazine').bind('touchstart', regionClick);
+		} else {
+		    $('.magazine').click(regionClick);
+		}
+		
+		// Events for the next button
+		$('.next-button').bind($.mouseEvents.over, function() {
+		    $(this).addClass('next-button-hover');
+		}).bind($.mouseEvents.out, function() {
+		    $(this).removeClass('next-button-hover');
+		}).bind($.mouseEvents.down, function() {
+		    $(this).addClass('next-button-down');
+		}).bind($.mouseEvents.up, function() {
+		    $(this).removeClass('next-button-down');
+		}).click(function() {
+		    $('.magazine').turn('next');
+		});
+		
+		// Events for the next button
+		$('.previous-button').bind($.mouseEvents.over, function() {
+		    $(this).addClass('previous-button-hover');
+		}).bind($.mouseEvents.out, function() {
+		    $(this).removeClass('previous-button-hover');
+		}).bind($.mouseEvents.down, function() {
+		    $(this).addClass('previous-button-down');
+		}).bind($.mouseEvents.up, function() {
+		    $(this).removeClass('previous-button-down');
+		}).click(function() {
+		    $('.magazine').turn('previous');
+		});
+		
+		
 	});
 </script>
 <script type="text/javascript">
