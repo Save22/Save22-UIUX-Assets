063952c35e0a43b6fde5ac58f7f086a141643d44
Camille Zapata <camillezapata@gmail.com>
2013-05-21 11:28:04 +0800

    updated establishment-branch page

--- Launchpad2/launchpad2-style1/establishment1-branch.php
+++ Launchpad2/launchpad2-style1/establishment1-branch.php
@@ -14,57 +14,79 @@
         
         <header class="row">
           <div class="small-12 columns">
-            <h2 class="left">Your Shopwise Branch Details</h2>
+            <h2 class="left">Shopwise Branch Details</h2>
 
             <div class="distance right">
               Distance: <strong>5km</strong> 
-              <a href="#" class="tiny button secondary inline">Map</a>
+              <a href="#" class="tiny button secondary inline" data-reveal-id="showMap">Map</a>
             </div>
           </div>
         </header>
 
-        <ul class="small-block-grid-1 large-block-grid-3 sub-content">
-          <li class="address">
-            <section>
-              <h3>Address</h3>
-              <p>2343 Street Name<br>
-                City or whatever</p>
-            </section>
-          </li>
+        <div class="row">
+          <div class="small-12 large-4 columns">
+            <img src="img/retailer-shopwise.jpg" alt="Shopwise" class="retailer-logo">
+          </div>
+          <div class="small-12 large-8 columns">
 
-          <li class="store-hours">
-            <section>
-              <h3>Store Hours</h3>
-              <p>10am - 11pm</p>
-            </section>
-          </li>
+            <ul class="small-block-grid-1 large-block-grid-2 sub-content">
+              <li class="address">
+                <section>
+                  <h3>Address</h3>
+                  <p>2343 Street Name<br>
+                    City or whatever</p>
+                </section>
+              </li>
 
-          <li class="contact-info">
-            <section>
-              <h3>Contact Info</h3>
-              <p>
-                <span class="label">Tel. No.</span> 234 - 2342 - 20055 <br>
-                <span class="label">Email</span> shop@shopwise.com 
-              </p>
-            </section>
-          </li>
+              <li class="store-hours">
+                <section>
+                  <h3>Store Hours</h3>
+                  <p>10am - 11pm</p>
+                </section>
+              </li>
 
-          <li class="pay-options">
-            <section>
-              <h3>Payment Options</h3>
-              <p>Cash<br>
-                Credit Card</p>
-            </section>
-          </li>
+              <li class="contact-info">
+                <section>
+                  <h3>Contact Info</h3>
+                  <p>
+                    <span class="label">Tel. No.</span> 234 - 2342 - 20055 <br>
+                    <span class="label">Email</span> shop@shopwise.com 
+                  </p>
+                </section>
+              </li>
 
+              <li class="pay-options">
+                <section>
+                  <h3>Payment Options</h3>
+                  <p>Cash<br>
+                    Credit Card</p>
+                </section>
+              </li>
 
-        </ul>
+            </ul>
+          </div>
+        </div>
+        <div class="section-container accordion" data-section="accordion">
+        <section class="description">
+          <p class="title" data-section-title><a href="#">About Shopwise</a></p>
+            <div class="content" data-section-content>
+
+              <p>Rustan Supercenters, Inc. (RSCI), a member of the Rustan Group of Companies, was founded in 1998 at the height of the Asian Economic Crisis. It was the Rustan Group's first major foray into the discount-retailing segment through an adapted European style hypermarket called Shopwise.  RSCI pioneered the hypermarket concept in the Philippines. </p>
+
+              <p>Armed with the vision of providing Quality for All , the Company sought to make the renowned Rustan's quality accessible to all, especially the middle and working class.  Its mission is to create a chain of supercenters or hypermarkets, which is the most preferred one-stop shop destination for fulfilling the basic needs of the Filipino family. </p>
+
+              <p>Rustan's decision to diversify into hypermarkets was borne out of manifest opportunities brought about by fundamental changes that have been taking place in the Philippine market:  a burgeoning middle class; increasing value consciousness across various income levels; and new geographical market opportunities that are best served through discount retailing operations.  RSCI developed and opened the first hypermarket in the country in November 29, 1998 in Filinvest Alabang.  It now operates nine hypermarkets and is set to open its first branch in Cebu on February 17, 2012.  Rustan Supercenters, Inc. also operates the Rustan's chain of supermarkets. </p>
+
+              <p>The highly successful Shopwise brand pioneered the hypermarket concept in the Philippines 14 years ago and is known for its affordable quality, everyday freshness, budget-friendly variety and fast, friendly and fun service. Millions of customers have made Shopwise their shopping destination of choice because it is the most comfortable, practical and efficient place to shop for a complete selection of fresh goods, groceries, clothes, toys and home accessories all under one roof.  Value- conscious customers flock to Shopwise to enjoy its attractive prices, wide aisles, clean surroundings, quality merchandise and the overall shopping experience.</p>
+            </div>
+          </section>
+        </div>
       </div>
 
       <section class="section results search-results results-list">
 
         <header>
-          <h2 class="main-header">Shopwise in Ortigas</h2>
+          <h2 class="main-header">Shopwise - Promotions and Branches</h2>
         </header>
 
         <div class="section-container tabs" data-section="tabs" data-options="deep_linking: true;">
@@ -76,31 +98,14 @@
                 <div class="small-12 large-3 columns">
                   <a href="#catalogue-URL"><img src="img/catsample5.jpg"></a>
                 </div>
-                <div class="small-12 large-5 columns">
+                <div class="small-12 large-9 columns">
                   <h2><a href="catalogue-URL">Catalogue Title</a></h2>
                   <h3>BRAND NAME</h3>
                   <p>
                     Valid until April 12, 2013
                   </p>
-
-                  <a href="#" class="button">Open Catalogue</a>
-
-                </div>
-                <div class="small-12 large-4 columns">
                   
-                  <a href="#" class="mobile-only view-shop-info drop-down">View Shop Info <span class="arrow arrow-down"></span></a>
-
-                  <section class="mobile-shop-info mobile-hide">
-                    <a href="#" class="store-link">
-                      <img src="img/retailer-shopwise.jpg" alt="Retailer Name">
-                    </a>
-                    <p>
-                      <strong>Nearest branch: 3.6km</strong> 
-                      <span class="map-link"><a href="#">[map]</a></span><br>
-                      Abenson Ortigas Ave.<br>
-                      <a href="#">All branches &rarr;</a><br>
-                    </p>
-                  </section>
+                  <a href="#" class="button">Open Catalogue</a>
 
                 </div>
               </div>
@@ -109,7 +114,7 @@
                 <div class="small-12 large-3 columns">
                   <a href="#catalogue-URL"><img src="img/catsample1.jpg"></a>
                 </div>
-                <div class="small-12 large-5 columns">
+                <div class="small-12 large-9 columns">
                   <h2><a href="catalogue-URL">Catalogue Title</a></h2>
                   <h3>BRAND NAME</h3>
                   <p>
@@ -118,23 +123,6 @@
                   
                   <a href="#" class="button">Open Catalogue</a>
 
-                </div>
-                <div class="small-12 large-4 columns">
-
-                  <a href="#" class="mobile-only view-shop-info drop-down">View Shop Info <span class="arrow arrow-down"></span></a>
-
-                  <section class="mobile-shop-info mobile-hide">
-                    <a href="#" class="store-link">
-                      <img src="img/retailer-shopwise.jpg" alt="Retailer Name">
-                    </a>
-                    <p>
-                      <strong>Nearest branch: 3.6km</strong> 
-                      <span class="map-link"><a href="#">[map]</a></span><br>
-                      Abenson Ortigas Ave.<br>
-                      <a href="#">All branches &rarr;</a><br>
-                    </p>
-                  </section>
-                  
                 </div>
               </div>
 
@@ -142,7 +130,7 @@
                 <div class="small-12 large-3 columns">
                   <a href="#catalogue-URL"><img src="img/catsample6.jpg"></a>
                 </div>
-                <div class="small-12 large-5 columns">
+                <div class="small-12 large-9 columns">
                   <h2><a href="catalogue-URL">Catalogue Title</a></h2>
                   <h3>BRAND NAME</h3>
                   <p>
@@ -151,23 +139,6 @@
                   
                   <a href="#" class="button">Open Catalogue</a>
 
-                </div>
-                <div class="small-12 large-4 columns">
-
-                  <a href="#" class="mobile-only view-shop-info drop-down">View Shop Info <span class="arrow arrow-down"></span></a>
-
-                  <section class="mobile-shop-info mobile-hide">
-                    <a href="#" class="store-link">
-                      <img src="img/retailer-shopwise.jpg" alt="Retailer Name">
-                    </a>
-                    <p>
-                      <strong>Nearest branch: 3.6km</strong> 
-                      <span class="map-link"><a href="#">[map]</a></span><br>
-                      Abenson Ortigas Ave.<br>
-                      <a href="#">All branches &rarr;</a><br>
-                    </p>
-                  </section>
-                  
                 </div>
               </div>
 
@@ -175,7 +146,7 @@
                 <div class="small-12 large-3 columns">
                   <a href="#catalogue-URL"><img src="img/catsample3.jpg"></a>
                 </div>
-                <div class="small-12 large-5 columns">
+                <div class="small-12 large-9 columns">
                   <h2><a href="catalogue-URL">Catalogue Title</a></h2>
                   <h3>BRAND NAME</h3>
                   <p>
@@ -184,23 +155,6 @@
                   
                   <a href="#" class="button">Open Catalogue</a>
 
-                </div>
-                <div class="small-12 large-4 columns">
-
-                  <a href="#" class="mobile-only view-shop-info drop-down">View Shop Info <span class="arrow arrow-down"></span></a>
-
-                  <section class="mobile-shop-info mobile-hide">
-                    <a href="#" class="store-link">
-                      <img src="img/retailer-shopwise.jpg" alt="Retailer Name">
-                    </a>
-                    <p>
-                      <strong>Nearest branch: 3.6km</strong> 
-                      <span class="map-link"><a href="#">[map]</a></span><br>
-                      Abenson Ortigas Ave.<br>
-                      <a href="#">All branches &rarr;</a><br>
-                    </p>
-                  </section>
-                  
                 </div>
               </div>
 
@@ -247,5 +201,22 @@
       -->
     </div>
   </div>
+
+
+
+<div id="showMap" class="reveal-modal">
+  <a class="close-reveal-modal">&#215;</a>
+
+  <section>
+    <h3>Shopwise, Ortigas</h3> <!-- SHOP NAME, CITY/LOCATION/AREA -->
+    <p>2343 Street Name<br>
+      City or whatever</p>
+  </section>
+
+  <section class="map">
+    <iframe width="100%" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Shopwise+Libis,+C-5,+Pasig+City,+Metro+Manila,+Philippines&amp;aq=0&amp;oq=shopwise+libis&amp;sll=37.0625,-95.677068&amp;sspn=48.50801,88.769531&amp;ie=UTF8&amp;hq=Shopwise+Libis,&amp;hnear=C-5,+Pasig+City,+Metro+Manila,+Philippines&amp;ll=14.603903,121.080502&amp;spn=0.058308,0.086689&amp;t=m&amp;z=14&amp;iwloc=A&amp;cid=4916592748317610912&amp;output=embed"></iframe><br /><small><a href="https://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=Shopwise+Libis,+C-5,+Pasig+City,+Metro+Manila,+Philippines&amp;aq=0&amp;oq=shopwise+libis&amp;sll=37.0625,-95.677068&amp;sspn=48.50801,88.769531&amp;ie=UTF8&amp;hq=Shopwise+Libis,&amp;hnear=C-5,+Pasig+City,+Metro+Manila,+Philippines&amp;ll=14.603903,121.080502&amp;spn=0.058308,0.086689&amp;t=m&amp;z=14&amp;iwloc=A&amp;cid=4916592748317610912" style="color:#0000FF;text-align:left">View Larger Map</a></small>
+  </section>
+</div>
+
 
 <?php include('footer.php'); ?>
