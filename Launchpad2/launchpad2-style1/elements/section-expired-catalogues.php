ed5df31c36103f2219a5043bc4fa83de27e31f05
Camille Zapata <camillezapata@gmail.com>
2013-07-29 13:07:46 +0800

    template for expired promos

---
+++ Launchpad2/launchpad2-style1/elements/section-expired-catalogues.php
@@ -0,0 +1,35 @@
+
+      <div class="section catalogues-expired panel secondary-content">
+        <header class="secondary-header">
+          <div class="row">
+            <div class="small-12 columns">
+              <h2 class="inline left">Expired Promotions</h2>
+              <a href="#" class="button dropdown secondary inline left" data-dropdown="archives"><span class="current">June 2013</span></a>
+              <ul id="archives" class="f-dropdown">
+                <li><a href="#">April 2013</a></li>
+                <li><a href="#">May 2013</a></li>
+                <li><a href="#">June 2013</a></li>
+                <li><a href="#">July 2013</a></li>
+              </ul>
+            </div>
+          </div>
+        </header>
+
+        <section>
+          <h3>Expired on June 2013</h3>
+          <ul class="small-block-grid-1 large-block-grid-3 sub-content">
+            <li><a href="#" class="expired-promo">BAGUS RAYA SALE at NOVENA</a><br><a href="#" class="retailer">Retailer Name</a></li>
+            <li><a href="#" class="expired-promo">SALE up to 50% at COMMUNE</a><br><a href="#" class="retailer">Retailer Name</a></li>
+            <li><a href="#" class="expired-promo">FREE NTUC VOUCHER WORTH UP TO $50 at IFC</a><br><a href="#" class="retailer">Retailer Name</a></li>
+            <li><a href="#" class="expired-promo">Limited time deals</a><br><a href="#" class="retailer">Retailer Name</a></li>
+            <li><a href="#" class="expired-promo">Repaint all the walls, or simply add a few dashes of colour?</a><br><a href="#" class="retailer">Retailer Name</a></li>
+            <li><a href="#" class="expired-promo">Make a big change at home.</a><br><a href="#" class="retailer">Retailer Name</a></li>
+            <li><a href="#" class="expired-promo">BAGUS RAYA SALE at NOVENA</a><br><a href="#" class="retailer">Retailer Name</a></li>
+            <li><a href="#" class="expired-promo">SALE up to 50% at COMMUNE</a><br><a href="#" class="retailer">Retailer Name</a></li>
+            <li><a href="#" class="expired-promo">FREE NTUC VOUCHER WORTH UP TO $50 at IFC</a><br><a href="#" class="retailer">Retailer Name</a></li>
+            <li><a href="#" class="expired-promo">Limited time deals</a><br><a href="#" class="retailer">Retailer Name</a></li>
+            <li><a href="#" class="expired-promo">Repaint all the walls, or simply add a few dashes of colour?</a><br><a href="#" class="retailer">Retailer Name</a></li>
+            <li><a href="#" class="expired-promo">Make a big change at home.</a><br><a href="#" class="retailer">Retailer Name</a></li>
+          </ul>
+        </section>
+      </div>
