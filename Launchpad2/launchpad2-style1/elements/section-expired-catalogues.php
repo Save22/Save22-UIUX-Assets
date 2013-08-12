7edfc91ac53b037449fcff75f0c8a64bdceb4b17
Camille Zapata <camillezapata@gmail.com>
2013-08-12 11:50:08 +0800

    changed footer links for expired promos

--- Launchpad2/launchpad2-style1/elements/section-expired-catalogues.php
+++ Launchpad2/launchpad2-style1/elements/section-expired-catalogues.php
@@ -1,38 +1,32 @@
 
       <div class="section catalogues-expired panel secondary-content">
         <header class="secondary-header">
-          <div class="row">
-            <div class="small-12 columns">
-              <h2 class="inline left">Expired Promotions</h2>
-              <a href="#" class="button dropdown secondary inline left" data-dropdown="archives"><span class="current">June 2013</span></a>
-              <ul id="archives" class="f-dropdown">
-                <li><a href="archive-expired.php">April 2013</a></li>
-                <li><a href="archive-expired.php">May 2013</a></li>
-                <li><a href="archive-expired.php">June 2013</a></li>
-                <li><a href="archive-expired.php">July 2013</a></li>
-              </ul>
-            </div>
-          </div>
+          <h2>Expired Promotions</h2>
         </header>
 
-        <section>
-          <h3>Expired on June 2013</h3>
-          <ul class="small-block-grid-1 large-block-grid-3 sub-content">
-            <li><a href="#" class="expired-promo">BAGUS RAYA SALE at NOVENA</a><br><a href="#" class="retailer">Retailer Name</a></li>
-            <li><a href="#" class="expired-promo">SALE up to 50% at COMMUNE</a><br><a href="#" class="retailer">Retailer Name</a></li>
-            <li><a href="#" class="expired-promo">FREE NTUC VOUCHER WORTH UP TO $50 at IFC</a><br><a href="#" class="retailer">Retailer Name</a></li>
-            <li><a href="#" class="expired-promo">Limited time deals</a><br><a href="#" class="retailer">Retailer Name</a></li>
-            <li><a href="#" class="expired-promo">Repaint all the walls, or simply add a few dashes of colour?</a><br><a href="#" class="retailer">Retailer Name</a></li>
-            <li><a href="#" class="expired-promo">Make a big change at home.</a><br><a href="#" class="retailer">Retailer Name</a></li>
-            <li><a href="#" class="expired-promo">BAGUS RAYA SALE at NOVENA</a><br><a href="#" class="retailer">Retailer Name</a></li>
-            <li><a href="#" class="expired-promo">SALE up to 50% at COMMUNE</a><br><a href="#" class="retailer">Retailer Name</a></li>
-            <li><a href="#" class="expired-promo">FREE NTUC VOUCHER WORTH UP TO $50 at IFC</a><br><a href="#" class="retailer">Retailer Name</a></li>
-            <li><a href="#" class="expired-promo">Limited time deals</a><br><a href="#" class="retailer">Retailer Name</a></li>
-            <li><a href="#" class="expired-promo">Repaint all the walls, or simply add a few dashes of colour?</a><br><a href="#" class="retailer">Retailer Name</a></li>
-            <li><a href="#" class="expired-promo">Make a big change at home.</a><br><a href="#" class="retailer">Retailer Name</a></li>
-          </ul>
+        <section><br>
+          <a href="archive-expired.php" class="button secondary view-all">View All Expired Supermarket Promos</a>
         </section>
         <section>
-          <a href="archive-expired.php" class="button secondary view-all">View All Expired Supermarket Promos</a>
+          <ul class="small-block-grid-1 large-block-grid-3 sub-content">
+            <li><a href="archive-expired.php">Category Name</a></li>
+            <li><a href="archive-expired.php">Category Name</a></li>
+            <li><a href="archive-expired.php">Category Name</a></li>
+            <li><a href="archive-expired.php">Category Name</a></li>
+            <li><a href="archive-expired.php">Category Name</a></li>
+            <li><a href="archive-expired.php">Category Name</a></li>
+            <li><a href="archive-expired.php">Category Name</a></li>
+            <li><a href="archive-expired.php">Category Name</a></li>
+            <li><a href="archive-expired.php">Category Name</a></li>
+            <li><a href="archive-expired.php">Category Name</a></li>
+            <li><a href="archive-expired.php">Category Name</a></li>
+            <li><a href="archive-expired.php">Category Name</a></li>
+            <li><a href="archive-expired.php">Category Name</a></li>
+            <li><a href="archive-expired.php">Category Name</a></li>
+            <li><a href="archive-expired.php">Category Name</a></li>
+            <li><a href="archive-expired.php">Category Name</a></li>
+            <li><a href="archive-expired.php">Category Name</a></li>
+            <li><a href="archive-expired.php">Category Name</a></li>
+          </ul>
         </section>
       </div>
