68f653ffaa457edd3568f20a3875624ddea68072
Camille Zapata <camillezapata@gmail.com>
2013-08-12 11:38:26 +0800

    changed the style for the expired archive links on sidebar

--- Launchpad2/launchpad2-style1/elements/sidebar-expired-archives.php
+++ Launchpad2/launchpad2-style1/elements/sidebar-expired-archives.php
@@ -1,30 +1,15 @@
 
         <nav class="list-links sidebar">
-          <h2 class="sidebar-header header-establishments">Expired Promos</h2>
+            <a href="#" class="large button dropdown secondary" data-dropdown="drop-expired-archive">Expired Promos</a>
 
-          <ul class="quick-links">
-            <li><a href="#">January 2013</a></li>
-            <li><a href="#">February 2013</a></li>
-            <li><a href="#">March 2013</a></li>
-            <li><a href="#">April 2013</a></li>
-            <li><a href="#">May 2013</a></li>
-            <li class="active"><a href="#" class="active">June 2013</a></li>
-            <li><a href="#">July 2013</a></li>
-            <li><a href="#">August 2013</a></li>
-            <li><a href="#">September 2013</a></li>
-            <li class="trigger"><a href="#" class="trigger-more-links">View More <span><img src="img/menu-arrow-down.svg"></span></a></li>
-          </ul>
-          <ul class="more-links hide">
-            <li><a href="#">January 2012</a></li>
-            <li><a href="#">February 2012</a></li>
-            <li><a href="#">March 2012</a></li>
-            <li><a href="#">April 2012</a></li>
-            <li><a href="#">May 2012</a></li>
-            <li><a href="#">June 2012</a></li>
-            <li><a href="#">July 2012</a></li>
-            <li><a href="#">August 2012</a></li>
-            <li><a href="#">September 2012</a></li>
-            <li class="trigger"><a href="#" class="trigger-close-menu">Close <span><img src="img/menu-arrow-up.svg"></span></a></li>
-          </ul>
+            <ul id="drop-expired-archive" class="f-dropdown">
+              <li><a href="#">January 2013</a></li>
+              <li><a href="#">February 2013</a></li>
+              <li><a href="#">March 2013</a></li>
+              <li><a href="#">April 2013</a></li>
+              <li><a href="#">May 2013</a></li>
+              <li><a href="#">June 2013</a></li>
+            </ul>
+
         </nav>
 
